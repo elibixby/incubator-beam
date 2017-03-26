c1a0fbecac1d76cb5b8e88d52bb7485ef45694d1
elibixby <elibixby@google.com>
2017-03-26 09:52:03 -0700

    Support fan in with generators

--- sdks/python/apache_beam/transforms/core.py
+++ sdks/python/apache_beam/transforms/core.py
@@ -19,6 +19,7 @@
 
 from __future__ import absolute_import
 
+import collections
 import copy
 import inspect
 import types
@@ -223,21 +224,25 @@ class GeneratorWrapperDoFn(DoFn):
     self._gen_kwargs = kwargs
 
   def element_generator(self):
-    while self._current_value is not None:
-      yield self._current_value
+    while self._element_queue is not None:
+      try:
+        yield self._element_queue.pop()
+      except IndexError as e:
+        raise StopIteration(str(e))
 
     raise StopIteration()
 
   def start_bundle(self, context):
+    self._element_queue = collections.deque()
     self._generator = self._generator_def(
         self.element_generator(), *self._gen_args, **self._gen_kwargs)
 
   def process(self, context, *args, **kwargs):
-    self._current_value = (context, args, kwargs)
+    self._element_queue.append((context, args, kwargs))
     return self._generator.next()
 
   def finish_bundle(self, context):
-    self._current_value = None
+    self._element_queue = None
     try:
       self._generator.next()
     except StopIteration:
